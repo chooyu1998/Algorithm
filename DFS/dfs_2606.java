7e72c6d71c7fd9fd2026a840dafd5e984628d7b9
chooyu1998 <choooyou98@gmail.com>
2022-03-26 03:45:30 +0900

    DFS

---
+++ DFS/dfs_2606.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+class DFS{
+	int V;
+	LinkedList<Integer> adj[];
+	boolean[] visited;
+	int res = -1;
+	DFS(int v){
+		V = v+1;
+		adj = new LinkedList[V];
+		visited = new boolean[V];
+		for(int i = 0; i < V; i++) {
+			adj[i] = new LinkedList();
+		}
+	}
+	
+	void AddEdge(int v, int w) {
+		adj[v].add(w);
+		adj[w].add(v);
+	}
+	void Find_Virus(int start) {
+		if(visited[start]) return;
+		else {
+			visited[start] = true;
+			res += 1;
+			LinkedList<Integer> stack = adj[start];
+			
+			for(Integer i : stack) {
+				Find_Virus(i);
+			}
+			
+			
+		}
+		
+	}
+}
+
+public class dfs_2606 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = "";
+		int node = Integer.parseInt(br.readLine());
+		int edge = Integer.parseInt(br.readLine());
+		
+		DFS dfs = new DFS(node);
+		for(int i = 0; i < edge; i++) {
+			str = br.readLine();
+			int node_1 = Integer.parseInt(str.split(" ")[0]);
+			int node_2 = Integer.parseInt(str.split(" ")[1]);
+			
+			dfs.AddEdge(node_1, node_2);
+		}
+		
+		
+		dfs.Find_Virus(1);
+		System.out.println(dfs.res);
+	}
+
+}
